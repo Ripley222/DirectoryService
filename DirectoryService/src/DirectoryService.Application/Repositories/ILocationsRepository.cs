c2f97131f92d58cab1eaeece0a075512462b0c04
Ripley222 <nik.petrunyak@bk.ru>
2025-08-27 10:21:31 +0300

    реализовал методы в репозитории для бизнес валидации(методы в dapper репозитории пока не работают)

--- DirectoryService/src/DirectoryService.Application/Repositories/ILocationsRepository.cs
+++ DirectoryService/src/DirectoryService.Application/Repositories/ILocationsRepository.cs
@@ -1,10 +1,15 @@
 ﻿using CSharpFunctionalExtensions;
 using DirectoryService.Domain.Entities.LocationEntity;
+using DirectoryService.Domain.Entities.LocationEntity.ValueObjects;
 using DirectoryService.Domain.Shared;
 
 namespace DirectoryService.Application.Repositories;
 
 public interface ILocationsRepository
 {
-    public Task<Result<Guid, Error>> Add(Location location, CancellationToken cancellationToken);
+    Task<Result<Guid, Error>> Add(Location location, CancellationToken cancellationToken);
+    
+    Task<Result<Location, Error>> GetByName(LocationName locationName, CancellationToken cancellationToken);
+    
+    Task<Result<Location, Error>> GetByAddress(Address address, CancellationToken cancellationToken);
 }
