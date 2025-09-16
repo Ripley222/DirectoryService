224ae890be72c7a5509e678cd568d6ca23bd78d6
Ripley222 <nik.petrunyak@bk.ru>
2025-09-16 09:27:51 +0300

    обновление конфигурации и миграций для работы с ltree и FromSql запросами

---
+++ DirectoryService/src/DirectoryService.Infrastructure/Migrations/20250903112849_AddLtreeProperty.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DirectoryService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLtreeProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:PostgresExtension:ltree", ",,");
+
+            migrationBuilder.Sql(
+                @"ALTER TABLE ""departments""
+                  ALTER COLUMN ""path"" TYPE ltree
+                  USING ""path""::ltree");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_departments_path",
+                table: "departments",
+                column: "path")
+                .Annotation("Npgsql:IndexMethod", "gist");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "idx_departments_path",
+                table: "departments");
+
+            migrationBuilder.AlterDatabase()
+                .OldAnnotation("Npgsql:PostgresExtension:ltree", ",,");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "path",
+                table: "departments",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "ltree");
+        }
+    }
+}
