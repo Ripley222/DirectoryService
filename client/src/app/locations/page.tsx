b8438b65d36ea9f83abda1da8591a2ea74da8887
Ripley222 <nik.petrunyak@bk.ru>
2026-01-07 12:05:52 +0300

    реализация вывода локаций

--- client/src/app/locations/page.tsx
+++ client/src/app/locations/page.tsx
@@ -1,8 +1,65 @@
+"use client"
+
+import Link from "next/link";
+import {useEffect, useState} from "react";
+import {locationsApi} from "@/entities/locations/api";
+import {LocationEntity} from "@/entities/locations/types";
+
+const PAGE_SIZE = 20;
+
 export default function LocationsPage() {
+    const [page, setPage] = useState(1)
+    const [locations, setLocations] = useState<LocationEntity[] | null>(null);
+
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        locationsApi
+            .getLocations({page, pageSize: PAGE_SIZE})
+            .then((data) => setLocations(data))
+            .catch((error) => setError(error.message));
+    }, [page]);
+
+    if (error) {
+        return <div>Ошибка: {error}</div>;
+    }
+
+    if (!locations) {
+        return <div>Loading...</div>;
+    }
+
     return (
-        <div>
-            <h1 className="text-2xl font-bold mb-4">Locations</h1>
-            <p>Список всех локаций организации.</p>
+        <div className="p-6">
+            <div>
+                <h1 className="text-2xl font-bold mb-4">Локации</h1>
+                <p>Список всех локаций где расположены подразделения</p>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {locations?.map((location) => (
+                    <Link
+                        key={location.locationId}
+                        href={`/locations/${location.locationId}`}
+                        className="rounded-xl border border-gray-200 bg-white p-5 shadow-sm hover:shadow-md transition">
+
+                        <h2 className="text-lg font-semibold mb-2">
+                            {location.locationName}
+                        </h2>
+
+                        <p className="text-sm text-gray-600 mb-1">
+                            {location.locationAddress}
+                        </p>
+
+                        <p className="text-sm text-gray-500">
+                            {location.locationTimeZone}
+                        </p>
+
+                        <p className="text-sm text-gray-500">
+                            {location.isActive ? "Локация активна" : "Локация удалена"}
+                        </p>
+                    </Link>
+                ))}
+            </div>
         </div>
     );
 }
---
+++ client/src/entities/locations/api.ts
@@ -0,0 +1,62 @@
+import {LocationEntity} from "@/entities/locations/types";
+import {apiClient} from "@/shared/api/axios-instance";
+
+export type CreateLocationRequest = {
+    name: string;
+    city: string;
+    street: string;
+    house: string;
+    room_number: string;
+    timeZone: string;
+}
+
+export type GetLocationsRequest = {
+    departmentIds?: string[],
+    search?: string,
+    isActive?: boolean,
+    page?: number,
+    pageSize?: number,
+    sortBy?: string,
+    sortDirection?: string,
+}
+
+export type Envelope<T = unknown> = {
+    result: T | null;
+    error: ApiError | null;
+    isError: boolean;
+    timeGenerated: string;
+}
+
+export type ApiError = {
+    messages: ErrorMassage[];
+    type: ErrorType;
+}
+
+export type ErrorMassage = {
+    code: string;
+    message: string;
+    invalidField?: string | null;
+}
+
+export type ErrorType =
+    | "validation"
+    | "not_found"
+    | "failure"
+    | "conflict";
+
+export const locationsApi = {
+    getLocations: async (request: GetLocationsRequest): Promise<LocationEntity[]> => {
+        const response = await apiClient
+            .get<Envelope<{ locations: LocationEntity[] }>>("locations", {
+                params: request
+            });
+
+        return response.data.result?.locations || [];
+    },
+
+    createLocations: async (request: CreateLocationRequest) => {
+        const response = await apiClient.post("locations", request);
+
+        return response.data;
+    }
+}
