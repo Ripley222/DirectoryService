3741107670b60fbcc6d87202bdda3c44efc7fb1a
Ripley222 <nik.petrunyak@bk.ru>
2026-01-08 10:53:02 +0300

    подключение QueryClient и QueryClientProvider

--- client/src/shared/api/axios-instance.ts
+++ client/src/shared/api/axios-instance.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const apiClient = axios.create({
     baseURL: "http://localhost:9001/api",
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json" }
 })
